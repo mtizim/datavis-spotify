5f52b552b267db933daeb934811725817dd33793
Mateusz Ziemła <mateusz.ziemla@leancode.pl>
2021-01-10 14:35:51 +0100

    Clickable songs

--- webpage/script.js
+++ webpage/script.js
@@ -1,5 +1,10 @@
-d3.csv("data.csv", function (data) {
-    for (var i = 0; i < data.length; i++) {
-        console.log("ball");
-    }
+d3.csv("data.csv", function (track) {
+
+    d3.select("#div1").append("a")
+        .attr("href", track["link"])
+        .append("img")
+        .attr("class", "track")
+
+        .attr("src", track["img"])
+        .attr("alt", track["name"]);
 });
