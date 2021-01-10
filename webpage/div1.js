002dd780e4fc73d3ae74871f1860c457fb885d22
Mateusz Ziemła <mateusz.ziemla@leancode.pl>
2021-01-10 15:43:03 +0100

    div1

--- webpage/div1.js
+++ webpage/div1.js
@@ -1,10 +1,11 @@
 d3.csv("data.csv", function (track) {
+    box = d3.select("#div1")
 
-    d3.select("#div1").append("a")
+    box.select("#div1" + track["whose"]).append("a")
         .attr("href", track["link"])
         .append("img")
         .attr("class", "track")
-
+        .attr("title", track["name"])
         .attr("src", track["img"])
         .attr("alt", track["name"]);
 });
